c88687e5b4becd3367e61131555b552a8950cafc
msobotka <milenasobotka@gmail.com>
2021-11-21 13:19:31 +0100

    on update

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ class MyClass:
             👋 ** Help **
             - On the left side of the screen there is a section with options to choose from
             - Select the object type for analysis - Image, Video or Live Camera view
-            - Next for Image or Video options load the selected file from the folder, for Live camera option click "Run" button
+            - Next for Image or Video options load the selected file from the folder, for Live Camera option click "Run" button
             - Choose how you want to display facial feature points
             - Press the "Face detection" button to detect a face in the image
-            - Using the "save plot with face detection" button you can save the plot with the obtained face detection on the image
+            - Using the "Save plot with face detection" button you can save the plot with the obtained face detection on the image
             - In order to use the face recognition functionality on an image already loaded on the page, open another file from the folder, which represents a photo of a person on the basis of which you want to use face recognition
             - Select HOG or CNN method
             - Enter the name of the person to be recognized
@@ -129,7 +129,7 @@ class MyClass:
         else:
             pass
 
-        if st.sidebar.button('save plot with face detection'):
+        if st.sidebar.button('Save plot with face detection'):
             pixels = pyplot.imread(self.UNKNOWN_IMAGE)
             decetor = MTCNN()
             faces = decetor.detect_faces(pixels)
@@ -147,8 +147,9 @@ class MyClass:
             self.LINES_SELECTED = True
 
     def reload_image(self, img):
-        image_location = st.empty()
-        image_location.image(img)
+        #image_location = st.empty()
+        #image_location.image(img)
+        st.image(img, use_column_width=True)
         # col1, col2, col3 = st.columns([0.5, 1, 0.5])
         # col2.image(img, use_column_width=False)
 
@@ -176,6 +177,36 @@ class MyClass:
         blur = cv2.blur(face, (42, 42))
         img[y:y + height, x:x + width] = blur
 
+    def detect_eyes(self, img):
+        face_landmark_list = face_recognition.face_landmarks(img)
+        for face_landmarks in face_landmark_list:
+            right_eye = []
+            left_eye = []
+
+            for i in range(6):
+                point_re = list(face_landmarks["right_eye"][i])
+                right_eye.append(point_re)
+                point_le = list(face_landmarks['left_eye'][i])
+                left_eye.append(point_le)
+
+            cv2.fillPoly(img, [np.array([right_eye], np.int32)], (0, 0, 0))
+            cv2.fillPoly(img, [np.array([left_eye], np.int32)], (0, 0, 0))
+
+    def detect_lips(self, img):
+        face_landmark_list = face_recognition.face_landmarks(img)
+        print(face_landmark_list)
+        for face_landmarks in face_landmark_list:
+            tlip = []
+            blip = []
+            for i in range(12):
+                point_re = list(face_landmarks["top_lip"][i])
+                tlip.append(point_re)
+                point_le = list(face_landmarks['bottom_lip'][i])
+                blip.append(point_le)
+
+            cv2.fillPoly(img, [np.array([tlip], np.int32)], (0, 0, 0))
+            cv2.fillPoly(img, [np.array([blip], np.int32)], (0, 0, 0))
+
     def face_detection_mtcnn(self, img, face_distance=None, person_name=None, display=True):
         pixels = pyplot.imread(img)
         decetor = MTCNN()
@@ -190,7 +221,15 @@ class MyClass:
 
         for face in faces:
             x, y, width, height = face['box']
+
+            if self.MASK:
+                #self.simple_mask(imageRGB, face['box'])
+                #self.detect_eyes(imageRGB)
+                #self.detect_lips(imageRGB)
+                self.FACE_IMAGE_MASKED = imageRGB.copy()
+
             cv2.rectangle(imageRGB, (x, y), (x + width, y + height), (255, 0, 0), 2)
+
             if face_distance is not None:
                 cv2.rectangle(imageRGB, (x, y + height), (x + width, y + height + 35), (255, 0, 0), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
@@ -215,21 +254,42 @@ class MyClass:
                     pil_image = Image.fromarray(imageRGB)
                     for face_landmarks in face_landmarks_list:
                         d = ImageDraw.Draw(pil_image, 'RGBA')
-                        d.line(face_landmarks['chin'], width=2)
-                        d.line(face_landmarks['left_eyebrow'], width=2)
-                        d.line(face_landmarks['right_eyebrow'], width=2)
-                        d.line(face_landmarks['nose_bridge'], width=2)
-                        d.line(face_landmarks['nose_tip'], width=2)
-                        d.line(face_landmarks['left_eye'], width=2)
-                        d.line(face_landmarks['right_eye'], width=2)
-                        d.line(face_landmarks['top_lip'], width=2)
-                        d.line(face_landmarks['bottom_lip'], width=2)
+                        #d.line(face_landmarks['chin'], width=2)
+                        #d.line(face_landmarks['left_eyebrow'], width=2)
+                        #d.line(face_landmarks['right_eyebrow'], width=2)
+                        #d.line(face_landmarks['nose_bridge'], width=2)
+                        #d.line(face_landmarks['nose_tip'], width=2)
+                        #d.line(face_landmarks['left_eye'], width=2)
+                        #d.line(face_landmarks['right_eye'], width=2)
+
+                        #d.line(face_landmarks['top_lip'], width=2)
+                        #d.line(face_landmarks['bottom_lip'], width=2)
+
+                        # Make the eyebrows into a nightmare
+                        d.polygon(face_landmarks['nose_tip'], fill=(255, 255, 255))
+                        #d.polygon(face_landmarks['nose_bridge'], fill=(0, 0, 0))
+                        #d.line(face_landmarks['nose_bridge'], fill=(0, 0, 0), width=4)
+                        d.line(face_landmarks['nose_tip'], fill=(255, 255, 255), width=4)
+
+                        # Gloss the lips
+                        d.polygon(face_landmarks['top_lip'], fill=(255, 255, 255))
+                        d.polygon(face_landmarks['bottom_lip'], fill=(255, 255, 255))
+                        d.line(face_landmarks['top_lip'], fill=(255, 255, 255), width=5)
+                        d.line(face_landmarks['bottom_lip'], fill=(255, 255, 255), width=4)
+
+                        # Sparkle the eyes
+                        d.polygon(face_landmarks['left_eye'], fill=(255, 255, 255))
+                        d.line(face_landmarks['left_eye'], fill=(255, 255, 255), width=10)
+                        d.polygon(face_landmarks['right_eye'], fill=(255, 255, 255))
+                        d.line(face_landmarks['right_eye'], fill=(255, 255, 255), width=10)
+
+                        # Apply some eyeliner
+                        d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(255, 255, 255),
+                               width=15)
+                        d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(255, 255, 255),
+                               width=15)
 
                     imageRGB = np.array(pil_image)
-
-            if self.MASK:
-                self.simple_mask(imageRGB, face['box'])
-                self.FACE_IMAGE_MASKED = imageRGB
 
         if display:
             self.reload_image(imageRGB)
